23086bbc2666d34e7985a0ee8f9ddb321c60ac12
Tim Jarratt <tjarratt@pivotal.io>
2020-12-01 21:26:11 +0100

    Day 1 part 2

--- day-01/first.go
+++ day-01/first.go
@@ -13,7 +13,64 @@ type intPair struct {
 	second int
 }
 
+type intTrio struct {
+	first  int
+	second int
+	third  int
+}
+
 func main() {
+	input := readInput()
+
+	resultChan := make(chan (intPair), 1)
+	for i := range input {
+		go checkForFirstDesiredResult(input[i], input, goal, resultChan)
+	}
+
+	result := <-resultChan
+	println("first answer:", result.first*result.second)
+
+	secondResultChan := make(chan (intTrio), 1)
+	for i := range input {
+		for j := range input {
+			given := intPair{first: input[i], second: input[j]}
+			go checkForSecondDesiredResult(given, input, goal, secondResultChan)
+		}
+	}
+
+	secondResult := <-secondResultChan
+	println("second answer:", secondResult.first*secondResult.second*secondResult.third)
+}
+
+// pragma mark - solutions
+func checkForFirstDesiredResult(given int, input []int, goal int, resultChan chan (intPair)) {
+	for _, i := range input {
+		if given+i != goal {
+			continue
+		}
+
+		resultChan <- intPair{first: given, second: i}
+		return
+	}
+}
+
+func checkForSecondDesiredResult(given intPair, input []int, goal int, resultChan chan (intTrio)) {
+	for _, num := range input {
+		if given.first+given.second+num != goal {
+			continue
+		}
+
+		resultChan <- intTrio{
+			first:  given.first,
+			second: given.second,
+			third:  num,
+		}
+		return
+	}
+}
+
+// pragma mark - private
+func readInput() []int {
 	stuff, err := ioutil.ReadFile("input-1.txt")
 	if err != nil {
 		panic(err)
@@ -32,24 +89,5 @@ func main() {
 		input = append(input, num)
 	}
 
-	resultChan := make(chan (intPair), 1)
-
-	for i := range input {
-		go checkForDesiredResult(input[i], input, goal, resultChan)
-	}
-
-	result := <-resultChan
-
-	println(result.first * result.second)
-}
-
-func checkForDesiredResult(given int, input []int, goal int, resultChan chan (intPair)) {
-	for _, i := range input {
-		if given+i != goal {
-			continue
-		}
-
-		resultChan <- intPair{first: given, second: i}
-		return
-	}
+	return input
 }
