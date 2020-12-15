892f4eb63afc73084213e66f884f182ca97c6c1c
Tim Jarratt <tjarratt@pivotal.io>
2020-12-15 12:19:41 +0100

    Day 15 part 1

---
+++ day-15/main.go
@@ -0,0 +1,9 @@
+package main
+
+import . "github.com/tjarratt/advent-of-code/2020/day-15/counting"
+
+func main() {
+	solver := NewCountingSolver("6,4,12,1,20,0,16")
+
+	println(solver.NumberSpokenAtTurn(2020))
+}
